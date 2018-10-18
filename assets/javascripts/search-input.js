8a89fe405d6d6cbd057ff60ddc48a2b911944653
Simon Márton <simon.marton@gmail.com>
2018-10-18 12:22:39 +0200

    feat(search): Added Algolia search. (#389)

--- assets/javascripts/search-input.js
+++ assets/javascripts/search-input.js
@@ -1,13 +1,55 @@
-var searchInput = document.getElementById('search-input');
-
-var sjs = SimpleJekyllSearch({
-	searchInput: searchInput,
-	resultsContainer: document.getElementById('results-container'),
-	json: '/search.json',
-	fuzzy: true
-});
-
 function clearSearchField() {
-	searchInput.value = "";
-	sjs.search("");
+	document.getElementById('search-input').value = "";
 }
+
+var client = algoliasearch('HI1538U2K4', 'f817c83dbdb923d880c215c9380d1107');
+var index = client.initIndex('devcenter');
+
+autocomplete(
+  '#search-input',
+  {
+    hint: false
+  },
+  {
+    source: autocomplete.sources.hits(index, { hitsPerPage: 5 }),
+    displayKey: 'title',
+    templates: {
+      suggestion: function(suggestion) {
+        var contentMatch = '';
+				var heading = '';
+				
+				if( suggestion.headings.length){
+					heading =  [
+            '<span class="aa-suggestion-heading"> &bull; ',
+            suggestion.headings[0],
+            '</span>'
+          ].join('');
+				} 
+
+        if (suggestion._snippetResult.content.matchLevel !== 'none') {
+          contentMatch = [
+            '<div class="aa-suggestion-content">',
+            suggestion._snippetResult.content.value,
+            '</div>'
+          ].join('');
+        }
+
+        return [
+          '<div class="aa-suggestion-title">',
+          suggestion._highlightResult.title.value,
+					heading,
+          '</div>',
+          contentMatch
+        ].join('');
+      }
+    }
+  }
+).on('autocomplete:selected', function(event, suggestion, dataset) {
+	var url = suggestion.url;
+
+	if(suggestion.anchor) {
+		url += '#' + suggestion.anchor;
+	}
+
+	location.href = url;
+});;
