ad405319e188aa290c48ed7ee99f622d7f9e4be9
Simon Márton <simon.marton@bitrise.io>
2019-04-10 14:20:51 +0200

    Fix/algolia (#439)
    * Fix algolia indexing
    
    * fix algolia url

--- _plugins/algolia_hooks.rb
+++ _plugins/algolia_hooks.rb
@@ -2,8 +2,8 @@ module Jekyll
   module Algolia
     module Hooks
       def self.before_indexing_each(record, node, context)
-        # We use hte urls without the beginning /en for the default language
-        record[:url] = record[:url].gsub(/^en\//, '') unless record[:url].nil?
+        # We use the urls without the beginning /en for the default language, also make sure it's absolute
+        record[:url] = record[:url].gsub(/^\/?(en\/)?/, '/') unless record[:url].nil?
 
         record
       end
