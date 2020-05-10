cc44a8adcf067c4716c31bf1eafcb40763e973a6
zoltan-baba <zoltan.baba@bitrise.io>
2020-05-10 14:40:54 +0200

    refactored side-menu code

--- assets/javascripts/side-menu.js
+++ assets/javascripts/side-menu.js
@@ -1,28 +1,50 @@
+
+
 function configureSideMenu() {
+  
+  const currentPage = window.location.href;
+  const menuOpeners = document.querySelectorAll("[class^=menu-list-opener]");
 
-  var currentPage = window.location.href;
+  function menuParent(el) {
+    el.parentElement.parentElement.previousElementSibling.classList.toggle('open', true);
+  }
 
-  $("#side-menu a").each(
-    function() {
-      if (this.href == currentPage && this.href.indexOf("index") != -1) {
-        $(this).addClass("open") && $(this.closest("ol").previousElementSibling).addClass("open");
+  function menuListOpener() {
+
+    for (i = 0; i < menuOpeners.length; i++) {
+      if (menuOpeners[i].href == currentPage) {
+        menuOpeners[i].classList.toggle("open");
       }
-
-      else if (this.href == currentPage && this.href.indexOf("index") == -1) {
-        var mainMenuItem =  this.closest("div.menu-wrapper > ol > li > ol").previousElementSibling;
-        $(this.closest("ol > li > ol").previousElementSibling).addClass("open") && $(mainMenuItem).addClass("open");
+      if (menuOpeners[i].href == currentPage && menuOpeners[i].classList.contains('menu-list-opener')) {
+        menuParent(menuOpeners[i]);
       }
     }
-  );
+  }
+  menuListOpener();
+  
+  function menuItemOpener() { 
+
+    const menuItems = document.querySelectorAll(".menu-link");
+
+    for (i = 0; i < menuItems.length; i++) {
+      let menuItemParent = menuItems[i].parentElement.parentElement.previousElementSibling; 
+      if (menuItems[i].href == currentPage) {
+        if (menuItemParent.classList.contains("menu-list-opener")) {
+          menuItems[i].classList.toggle("current", true), menuParent(menuItems[i]), menuParent(menuItemParent);
+        }
+        else {
+          menuItems[i].classList.toggle("current", true), menuParent(menuItems[i]);
+        }
+      }
+    }
+  }
+  menuItemOpener();
+
 }
 
 function toggleSideBar() {
 	var sideMenu = document.getElementById('side-menu');
 	sideMenu.classList.toggle("open");
-}
-
-function toggleMenuListOpener(event) {
-	event.target.classList.toggle("open");
 }
 
 configureSideMenu();
