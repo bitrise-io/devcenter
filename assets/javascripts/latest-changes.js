88c42ae4e11c6681659d706563300a2e60057155
zoltan-baba <zoltan.baba@bitrise.io>
2020-05-10 10:35:19 +0200

    removing bloat

--- assets/javascripts/latest-changes.js
+++ assets/javascripts/latest-changes.js
@@ -3,27 +3,22 @@ function getText(changeContent) {
 }
 
 function sortElements(nodeList) {
-  // Assume there's a common parent
   let node, parentNode = nodeList[0].parentNode;
 
-  // Define a sort function
   function sortEls(a, b) {
     let aText = getText(a);
     let bText = getText(b);
     return aText == bText? 0 : bText < aText? -1 : 1;
   }
 
-  // Convert nodelist to an array and remove from the DOM at the same time
   var a = [], i = nodeList.length;
   while (i--) {
     a[i] = parentNode.removeChild(nodeList[i]);
 
   }
 
-  // Sort the array
   a.sort(sortEls);
 
-  // Put elements back in order
   i = 0;
   while (node = a[i++]) {
     parentNode.appendChild(node);
