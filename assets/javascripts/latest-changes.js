a1f3a313e847150b6d339ac2e98c10c597bfdcf4
J. Doe (https://devcenter.bitrise.io/builds/setting-your-git-credentials-on-build-machines/) <please-set-your-email@bitrise.io>
2020-05-11 09:11:11 +0000

    new deploy

--- assets/javascripts/latest-changes.js
+++ assets/javascripts/latest-changes.js
@@ -1,13 +1,28 @@
-function sortArticles() {
-    var alphabeticallyOrderedDivs = $('.changelog-content').sort(function(a, b) {
-      var $aTitle = $(a).find('.changelog-content-title'), $bTitle = $(b).find('.changelog-content-title');
-      return String.prototype.localeCompare.call($bTitle.text().toLowerCase(), $aTitle.text().toLowerCase());
-    });
-  
-    var container = $(".changelog-container");
-    var containerTitle = $(".changelog-container-title");
-    container.detach().empty().prepend(containerTitle).append(alphabeticallyOrderedDivs);
-    $('main').append(container);
+function getText(changeContent) {
+  return changeContent.textContent || changeContent.innerText || '';
+}
+
+function sortElements(nodeList) {
+  let node, parentNode = nodeList[0].parentNode;
+
+  function sortEls(a, b) {
+    let aText = getText(a);
+    let bText = getText(b);
+    return aText == bText? 0 : bText < aText? -1 : 1;
   }
-  
-  sortArticles();
+
+  var a = [], i = nodeList.length;
+  while (i--) {
+    a[i] = parentNode.removeChild(nodeList[i]);
+
+  }
+
+  a.sort(sortEls);
+
+  i = 0;
+  while (node = a[i++]) {
+    parentNode.appendChild(node);
+  }
+}
+
+sortElements(document.querySelectorAll(".changelog-content"));
--- deploy.md
+++ deploy.md
@@ -9,4 +9,4 @@ new_article: false
 changelog: ''
 
 ---
-Deploy 202
+Deploy 203
