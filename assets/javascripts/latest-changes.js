e48a3618ebec3ba9c252ca88861528a2263eb3a7
Laszlo Moczo <laszlo.moczo@bitrise.io>
2021-10-08 15:14:27 +0200

    Optimize images

--- assets/javascripts/latest-changes.js
+++ assets/javascripts/latest-changes.js
@@ -1,28 +1,29 @@
 function getText(changeContent) {
-  return changeContent.textContent || changeContent.innerText || '';
+  return changeContent.textContent || changeContent.innerText || ''
 }
 
 function sortElements(nodeList) {
-  let node, parentNode = nodeList[0].parentNode;
+  var node,
+    parentNode = nodeList[0].parentNode
 
   function sortEls(a, b) {
-    let aText = getText(a);
-    let bText = getText(b);
-    return aText == bText? 0 : bText < aText? -1 : 1;
+    var aText = getText(a)
+    var bText = getText(b)
+    return aText == bText ? 0 : bText < aText ? -1 : 1
   }
 
-  var a = [], i = nodeList.length;
+  var a = [],
+    i = nodeList.length
   while (i--) {
-    a[i] = parentNode.removeChild(nodeList[i]);
-
+    a[i] = parentNode.removeChild(nodeList[i])
   }
 
-  a.sort(sortEls);
+  a.sort(sortEls)
 
-  i = 0;
-  while (node = a[i++]) {
-    parentNode.appendChild(node);
+  i = 0
+  while ((node = a[i++])) {
+    parentNode.appendChild(node)
   }
 }
 
-sortElements(document.querySelectorAll(".changelog-content"));
+sortElements(document.querySelectorAll('.changelog-content'))
--- assets/javascripts/side-menu.js
+++ assets/javascripts/side-menu.js
@@ -1,48 +1,53 @@
 function configureSideMenu() {
-  
-  const currentPage = window.location.href;
-  const menuOpeners = document.querySelectorAll("[class^=menu-list-opener]");
+  var currentPage = window.location.href
+  var menuOpeners = document.querySelectorAll('[class^=menu-list-opener]')
 
   function menuParent(el) {
-    el.parentElement.parentElement.previousElementSibling.classList.toggle('open', true);
+    el.parentElement.parentElement.previousElementSibling.classList.toggle(
+      'open',
+      true
+    )
   }
 
   function menuListOpener() {
-
     for (i = 0; i < menuOpeners.length; i++) {
       if (menuOpeners[i].href == currentPage) {
-        menuOpeners[i].classList.toggle("open");
+        menuOpeners[i].classList.toggle('open')
       }
-      if (menuOpeners[i].href == currentPage && menuOpeners[i].classList.contains('menu-list-opener')) {
-        menuParent(menuOpeners[i]);
+      if (
+        menuOpeners[i].href == currentPage &&
+        menuOpeners[i].classList.contains('menu-list-opener')
+      ) {
+        menuParent(menuOpeners[i])
       }
     }
   }
-  menuListOpener();
-  
-  function menuItemOpener() { 
+  menuListOpener()
 
-    const menuItems = document.querySelectorAll(".menu-link");
+  function menuItemOpener() {
+    var menuItems = document.querySelectorAll('.menu-link')
 
     for (i = 0; i < menuItems.length; i++) {
-      let menuItemParent = menuItems[i].parentElement.parentElement.previousElementSibling; 
+      var menuItemParent =
+        menuItems[i].parentElement.parentElement.previousElementSibling
       if (menuItems[i].href == currentPage) {
-        if (menuItemParent.classList.contains("menu-list-opener")) {
-          menuItems[i].classList.toggle("current", true), menuParent(menuItems[i]), menuParent(menuItemParent);
-        }
-        else {
-          menuItems[i].classList.toggle("current", true), menuParent(menuItems[i]);
+        if (menuItemParent.classList.contains('menu-list-opener')) {
+          menuItems[i].classList.toggle('current', true),
+            menuParent(menuItems[i]),
+            menuParent(menuItemParent)
+        } else {
+          menuItems[i].classList.toggle('current', true),
+            menuParent(menuItems[i])
         }
       }
     }
   }
-  menuItemOpener();
-
+  menuItemOpener()
 }
 
 function toggleSideBar() {
-	var sideMenu = document.getElementById('side-menu');
-	sideMenu.classList.toggle("open");
+  var sideMenu = document.getElementById('side-menu')
+  sideMenu.classList.toggle('open')
 }
 
-configureSideMenu();
+configureSideMenu()
