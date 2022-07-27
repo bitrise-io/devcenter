8c336bd8040303f77cabbc7bfb81ed3bb2a5b251
Simon Márton <simon.marton@bitrise.io>
2022-07-27 12:02:38 +0200

    formatting + gtm snippet

--- js/layout-custom-script.js
+++ js/layout-custom-script.js
@@ -1,136 +1,138 @@
 function initialConfig() {
+  const pageUrl = window.location.href;
+  const homePage = 'https://devcenter.bitrise.io/';
+  const jpHomePage = 'https://devcenter.bitrise.io/ja/index-ja.html';
+  const feedbackPanel = document.querySelector('div.feedback-panel');
 
-    const pageUrl = window.location.href;
-    const homePage = "https://devcenter.bitrise.io/";
-    const jpHomePage = "https://devcenter.bitrise.io/ja/index-ja.html";
-    const feedbackPanel = document.querySelector("div.feedback-panel");
+  let kbWrapper = document.createElement('div');
+  let kbCta = document.createElement('a');
 
-    let kbWrapper = document.createElement('div')
-    let kbCta = document.createElement('a');
+  kbWrapper.setAttribute('class', 'kb-wrapper');
 
-    kbWrapper.setAttribute("class", "kb-wrapper");
+  kbCta.setAttribute('class', 'kb-cta');
+  kbCta.setAttribute(
+    'href',
+    'https://support.bitrise.io/hc/en-us/categories/360000108597-Knowledge-Data-Base-?utm_source=devcenter&utm_medium=bottom_cta'
+  );
+  kbCta.setAttribute('target', '_blank');
+  kbCta.textContent = 'Visit our Knowledge Base!';
 
-    kbCta.setAttribute("class", "kb-cta");
-    kbCta.setAttribute("href", "https://support.bitrise.io/hc/en-us/categories/360000108597-Knowledge-Data-Base-?utm_source=devcenter&utm_medium=bottom_cta");
-    kbCta.setAttribute("target", "_blank");
-    kbCta.textContent = "Visit our Knowledge Base!";
+  let intCta = kbCta.cloneNode();
+  intCta.setAttribute('class', 'int-cta');
+  intCta.setAttribute('href', 'https://bitrise.io/integrations');
+  intCta.textContent = 'Check out all Bitrise Steps!';
 
-    let intCta = kbCta.cloneNode();
-    intCta.setAttribute ("class", "int-cta");
-    intCta.setAttribute("href", "https://bitrise.io/integrations");
-    intCta.textContent = "Check out all Bitrise Steps!";
+  kbWrapper.appendChild(kbCta);
+  kbWrapper.appendChild(intCta);
 
-    kbWrapper.appendChild(kbCta);
-    kbWrapper.appendChild(intCta);
+  feedbackPanel.insertAdjacentElement('beforebegin', kbWrapper);
 
-    feedbackPanel.insertAdjacentElement("beforebegin", kbWrapper);
+  function advSearchAdd() {
+    let searchBar = document.querySelector('.tool-search');
+    let searchParent = searchBar.parentNode;
+    searchParent.insertBefore(searchBar, searchParent.firstChild.nextSibling);
 
-    function advSearchAdd() {
-        let searchBar = document.querySelector(".tool-search");
-        let searchParent = searchBar.parentNode;
-        searchParent.insertBefore(searchBar, searchParent.firstChild.nextSibling);
+    let newDiv = document.createElement('div');
+    newDiv.setAttribute('class', 'adv-search');
 
-        let newDiv = document.createElement('div');
-        newDiv.setAttribute("class", "adv-search");
-    
-        let newA = document.createElement('a');
-        newA.setAttribute("href", "/search.html");
-        newA.setAttribute("class", "adv-search-link");
-        newA.setAttribute("target", "_blank");
-        newA.textContent = "Advanced search";
-    
-        newDiv.appendChild(newA);
-        searchBar.insertAdjacentElement("afterend", newDiv);
-    };
+    let newA = document.createElement('a');
+    newA.setAttribute('href', '/search.html');
+    newA.setAttribute('class', 'adv-search-link');
+    newA.setAttribute('target', '_blank');
+    newA.textContent = 'Advanced search';
 
-    
-    function langSwitcher() {
+    newDiv.appendChild(newA);
+    searchBar.insertAdjacentElement('afterend', newDiv);
+  }
 
-        let dropdownContainer = document.createElement('div');
-        dropdownContainer.setAttribute("class", "dropdown-container");
+  function langSwitcher() {
+    let dropdownContainer = document.createElement('div');
+    dropdownContainer.setAttribute('class', 'dropdown-container');
 
-        let menuText = document.createElement('span');
-        dropdownContainer.appendChild(menuText);
+    let menuText = document.createElement('span');
+    dropdownContainer.appendChild(menuText);
 
-        let dropdownContent = document.createElement('ul');
-        dropdownContent.setAttribute("class", "dropdown-content");
-    
-        let langOptionJa = document.createElement('a');
-        langOptionJa.setAttribute("href", "https://devcenter.bitrise.io/ja/index-ja.html");
-        langOptionJa.setAttribute("hreflang", "ja");
-        langOptionJa.textContent = "日本語"
-    
-        let langOptionEn = document.createElement('a');
-        langOptionEn.setAttribute("href", "https://devcenter.bitrise.io/");
-        langOptionEn.setAttribute("hreflang", "en");
-        langOptionEn.textContent = "EN"
-    
-        let liItemEn = document.createElement('li');
-        liItemEn.setAttribute("class", "lang-option");
-        let liItemJa = liItemEn.cloneNode();
-    
-        liItemEn.appendChild(langOptionEn);
-        liItemJa.appendChild(langOptionJa);
-    
-        dropdownContent.appendChild(liItemEn);
-        dropdownContent.appendChild(liItemJa);
-        dropdownContainer.appendChild(dropdownContent);
-        document.querySelector(".adv-search").insertAdjacentElement("afterend", dropdownContainer);
-    
-        if (pageUrl.indexOf("/ja/") > -1) {
-            liItemJa.classList.add("active-lang");
-            menuText.textContent = "日本語";
-        }
-        else if (pageUrl.indexOf("/en/") > -1) {
-            liItemEn.classList.add("active-lang");
-            menuText.textContent = "EN";
-        }
+    let dropdownContent = document.createElement('ul');
+    dropdownContent.setAttribute('class', 'dropdown-content');
 
-        else {
-            liItemEn.classList.add("active-lang");
-            menuText.textContent = "EN";
-        }
-    
-        let jaUrl = pageUrl.replace("/en/", "/ja/");
-        let enUrl = pageUrl.replace("/ja/", "/en/");
-  
-        if (pageUrl !== homePage && pageUrl !== jpHomePage && pageUrl.indexOf("/en/") > -1) {
-            langOptionJa.setAttribute("href", jaUrl);
-            langOptionEn.setAttribute("href", pageUrl);
-        }
+    let langOptionJa = document.createElement('a');
+    langOptionJa.setAttribute('href', 'https://devcenter.bitrise.io/ja/index-ja.html');
+    langOptionJa.setAttribute('hreflang', 'ja');
+    langOptionJa.textContent = '日本語';
 
-        else if (pageUrl !== homePage && pageUrl !== jpHomePage && pageUrl.indexOf("/ja/") > -1) {
-            langOptionJa.setAttribute("href", pageUrl);
-            langOptionEn.setAttribute("href", enUrl);
-        }
+    let langOptionEn = document.createElement('a');
+    langOptionEn.setAttribute('href', 'https://devcenter.bitrise.io/');
+    langOptionEn.setAttribute('hreflang', 'en');
+    langOptionEn.textContent = 'EN';
 
-        function toggleDropdown() {
-            dropdownContent.classList.toggle("show");
-        }
+    let liItemEn = document.createElement('li');
+    liItemEn.setAttribute('class', 'lang-option');
+    let liItemJa = liItemEn.cloneNode();
 
-        dropdownContainer.addEventListener("click", toggleDropdown);
-    };
+    liItemEn.appendChild(langOptionEn);
+    liItemJa.appendChild(langOptionJa);
 
+    dropdownContent.appendChild(liItemEn);
+    dropdownContent.appendChild(liItemJa);
+    dropdownContainer.appendChild(dropdownContent);
+    document.querySelector('.adv-search').insertAdjacentElement('afterend', dropdownContainer);
 
-    advSearchAdd();
-    langSwitcher();
+    if (pageUrl.indexOf('/ja/') > -1) {
+      liItemJa.classList.add('active-lang');
+      menuText.textContent = '日本語';
+    } else if (pageUrl.indexOf('/en/') > -1) {
+      liItemEn.classList.add('active-lang');
+      menuText.textContent = 'EN';
+    } else {
+      liItemEn.classList.add('active-lang');
+      menuText.textContent = 'EN';
+    }
 
-    /* Adding cookie consent banner scripts */
+    let jaUrl = pageUrl.replace('/en/', '/ja/');
+    let enUrl = pageUrl.replace('/ja/', '/en/');
 
-    let scriptOne = document.createElement('script');
-    scriptOne.src = "https://cdn.cookielaw.org/consent/a8f28a53-d138-4cac-a6dc-bdafd1611beb/OtAutoBlock.js";
-    document.head.appendChild(scriptOne);
-    
-    let scriptTwo = document.createElement('script');      
-    scriptTwo.src = "https://cdn.cookielaw.org/scripttemplates/otSDKStub.js"; 
-    scriptTwo.setAttribute("data-domain-script", "a8f28a53-d138-4cac-a6dc-bdafd1611beb");
-    document.head.appendChild(scriptTwo);
+    if (pageUrl !== homePage && pageUrl !== jpHomePage && pageUrl.indexOf('/en/') > -1) {
+      langOptionJa.setAttribute('href', jaUrl);
+      langOptionEn.setAttribute('href', pageUrl);
+    } else if (pageUrl !== homePage && pageUrl !== jpHomePage && pageUrl.indexOf('/ja/') > -1) {
+      langOptionJa.setAttribute('href', pageUrl);
+      langOptionEn.setAttribute('href', enUrl);
+    }
 
-    /* changing the feedback URL */
+    function toggleDropdown() {
+      dropdownContent.classList.toggle('show');
+    }
 
-    let feedbackLink = document.querySelector('#email-feedback > p > a');
-    feedbackLink.setAttribute("href", "https://www.bitrise.io/");
-    feedbackLink.setAttribute("target", "_blank");
-};
+    dropdownContainer.addEventListener('click', toggleDropdown);
+  }
+
+  advSearchAdd();
+  langSwitcher();
+
+  /* Adding cookie consent banner scripts */
+
+  let scriptOne = document.createElement('script');
+  scriptOne.src = 'https://cdn.cookielaw.org/consent/a8f28a53-d138-4cac-a6dc-bdafd1611beb/OtAutoBlock.js';
+  document.head.appendChild(scriptOne);
+
+  let scriptTwo = document.createElement('script');
+  scriptTwo.src = 'https://cdn.cookielaw.org/scripttemplates/otSDKStub.js';
+  scriptTwo.setAttribute('data-domain-script', 'a8f28a53-d138-4cac-a6dc-bdafd1611beb');
+  document.head.appendChild(scriptTwo);
+
+  /* changing the feedback URL */
+
+  let feedbackLink = document.querySelector('#email-feedback > p > a');
+  feedbackLink.setAttribute('href', 'https://www.bitrise.io/');
+  feedbackLink.setAttribute('target', '_blank');
+}
+
+// GTM
+const gtmContainerId = 'GTM-TZK32GR';
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer',gtmContainerId);
 
 initialConfig();
