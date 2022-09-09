a2c687a13fcb5f511ebe44dde8b19e7041ea220e
cdhenley219-bit <chanel.henley@bitrise.io>
2022-09-09 15:48:03 -0400

    Added data layer info to GTM reference

--- js/layout-custom-script.js
+++ js/layout-custom-script.js
@@ -125,13 +125,33 @@ function initialConfig() {
     feedbackLink.setAttribute('href', 'https://www.bitrise.io/');
     feedbackLink.setAttribute('target', '_blank');
   }
-  
+
   // GTM
   const gtmContainerId = 'GTM-TZK32GR';
-  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer',gtmContainerId);
+  (function(w,d,s,l,i){
+    function pick(fields, obj) {
+			return fields.reduce(function (picked, field) {
+				picked[field] = obj[field];
+				return picked;
+			}, {});
+		}
+
+    w[l]=w[l]||[];
+
+    /* Add fields to GTM dataLayer */
+    var dataLayerCookie = document.cookie.split('; ').find(row => row.startsWith('bDL='));
+    if (dataLayerCookie) {
+      var cookieInfo = decodeURIComponent(dataLayerCookie.split('=')[1]);
+      var parsedInfo = JSON.parse(cookieInfo);
+      var validEvents = Object.keys(parsedInfo).filter(function(event) {return Boolean(parsedInfo[event])});
+      w[l].push(pick(validEvents, parsedInfo));
+    }
+    
+    w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+    var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),
+        dl=l!='dataLayer'?'&l='+l:'';
+        j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer',gtmContainerId);
   
   initialConfig();
