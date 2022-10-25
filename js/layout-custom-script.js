c7bc0e313c148df5d1c603d084f381a4134e1871
roland-spekker-bitrise <roland.spekker@bitrise.io>
2022-10-25 09:44:35 +0200

    fix: manual override of OneTrust categorization to allow gtm.js to load - PLG-286

--- js/layout-custom-script.js
+++ js/layout-custom-script.js
@@ -126,7 +126,10 @@ const gtmContainerId = 'GTM-TZK32GR';
 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;
+// NOTE: Manual OneTrust categorization override to unblock gtm.js loading
+j.setAttribute('class','optanon-category-C0002'); 
+f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer',gtmContainerId);
 
 initialConfig();
