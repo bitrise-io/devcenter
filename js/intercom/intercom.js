ea037f54fe167ea72fa2bbecde3cad69a22f5637
Antal Orcsik <aorcsik@gmail.com>
2025-02-04 14:25:16 +0100

    handle intercom top banner

--- js/intercom/intercom.js
+++ js/intercom/intercom.js
@@ -31,4 +31,12 @@ window.intercomSettings = {
             w.addEventListener('load', l, false);
         }
     }
-})()
+})();
+
+// handle top banner layout changes
+window.setInterval(() => {
+    const bodyMarginTop = document.body.style.marginTop;  // intercom sets this for top banner
+    document.querySelector('aside.site-sidebar').style.marginTop = bodyMarginTop;
+    document.querySelector('aside.site-sidebar').style.maxHeight = `calc(100% - ${bodyMarginTop})`;
+    document.querySelector('.site-content .toolbar').style.marginTop = bodyMarginTop;
+}, 500);
